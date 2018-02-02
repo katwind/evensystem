5f38b626234da4e8631fe9ac1392b0ef7044411e
wind <katherinne@list.ru>
2018-02-02 23:32:26 +0300

    #11 implemented app startfile

--- app/src/app.java
+++ app/src/app.java
@@ -1,5 +1,25 @@
-/**
- * Created by Wind on 02.02.2018.
- */
-public class app {
+import UI.UsersController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import manager.DataManager;
+import model.User;
+import java.util.Scanner;
+
+public class app extends Application {
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("UI//users.fxml"));
+        primaryStage.setTitle("Личные данные");
+        primaryStage.setScene(new Scene(root, 600, 420));
+        primaryStage.show();
+    }
+
 }
