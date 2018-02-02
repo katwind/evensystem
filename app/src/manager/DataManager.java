1268f3cb4fb526fb53b2d0ab696c33bb47643458
wind <katherinne@list.ru>
2018-02-02 23:19:39 +0300

    #7 DataManager Implemented

--- app/src/manager/DataManager.java
+++ app/src/manager/DataManager.java
@@ -1,7 +1,60 @@
 package manager;
 
-/**
- * Created by Wind on 02.02.2018.
- */
+import model.User;
+import model.Form;
+
 public class DataManager {
+
+    User user;
+    Form form;
+
+    private static DataManager instance;
+
+    private DataManager() {
+
+    }
+
+    public static DataManager getInstance() {
+        if (instance == null)
+            instance = new DataManager();
+        return instance;
+    }
+
+    public void createNewUser(String fname, String sname, String tname, String address, String tel,
+                              String sex, String birthdate, String passseries, String passnum,
+                              String passgiven, String passdate, String passpers, String position,
+                              String education, String experience, String functions, String fstatus,
+                              String children, String hobbies, String additional) {
+
+        this.user = (new User.Builder())
+                .setFname(fname)
+                .setSname(sname)
+                .setTname(tname)
+                .setAddress(address)
+                .setTel(tel)
+                .setSex(sex)
+                .setBirthdate(birthdate)
+                .setPassseries(passseries)
+                .setPassnumber(passnum)
+                .setPassgiven(passgiven)
+                .setPassdate(passdate)
+                .setPasspers(passpers)
+                .setPosition(position)
+                .setEducation(education)
+                .setExperience(experience)
+                .setFunctions(functions)
+                .setFamilystatus(fstatus)
+                .setChildren(children)
+                .setHobbies(hobbies)
+                .setAdditional(additional)
+                .build();
+    }
+
+    public void addUser(User u) {
+        this.user = u;
+    }
+
+    public void addForm(User u) {
+        this.user = u;
+    }
 }
